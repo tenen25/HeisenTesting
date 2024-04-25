ffdde222da6189073f653292fa6163970acd7f13
tenen25 <75997072+tenen25@users.noreply.github.com>
2024-04-25 21:22:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+#Needed libraries:
 import math
 import numpy as np
 import time
@@ -5,21 +7,21 @@ from Aux_0 import *
 from Aux_Check import *
 
 
-
-
-
+# Needed paramaters from the user are: 
+# the stretch factor 'lamb',
+# the iteration number 'N',
+# the known testing domain 'known_set',
+# the suspected testing domain 'init_set'
 
 
 # Set the underlying stretch factor
 lamb = 3
-# Set the iteration number for the algorithm
-N = 1
-
-
-print('Computation is performed for lamb='+ str(lamb)+', N='+ str(N) +".")
 
 
 
+            
+
+#################### Auxiliary computations #####################
 
 # We define the dilation vector
 D = np.array([lamb, lamb, lamb**2])
@@ -27,18 +29,17 @@ D = np.array([lamb, lamb, lamb**2])
 V = [np.array([0, 0, 0])]
 # T will be the suspected testing domain
 
-            
 
-#################### Aux computations #####################
- 
-
+# Coordinates for the XY projection of the boxes
 x_start = -2
 x_end = 0
 
+# The following two sets are sets which we hope are smaller
+# testing domains
 T = GenerateFaces(l_1*2, l_2*2,x_start,x_end)
-
 T_3 = GenerateFaces(-2*2, 2*2,x_start,x_end)
 
+#The following are sets which we can show are testing domains
 T_2 = GenerateFaces(-3*2, 3*2,x_start,x_end)
 
 T_1 = GenerateFaces(-4*2, 4*2,x_start,x_end)
@@ -47,16 +48,19 @@ T_1 = GenerateFaces(-4*2, 4*2,x_start,x_end)
 K = DilatEdge(lamb)
 
 # K3 is known theoretic testing domain for 'lamb' 3
-K3 = FundemFaceIter(lamb, 2, V)
+K3 = FundemFaceIter(3, 2, V)
 
 #########################################################
 
 
 #################### User input #########################
 
+# Set the iteration number for the algorithm
+N = 1
+
 #Plug in the suspected testing domain
 init_set = T_3                   # set which we see whether is testing domain
-#W = FundemFaceIter(lamb, N, K)
+
 
 #Plug in the known testing domain
 known_set = T_2               # set which is 'known' to be a testing domain
